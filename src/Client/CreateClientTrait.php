aebdf6c5a66ec8249358089f518064ec0c1da6cd
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2021-09-30 23:01:19 +0200

    Downgrade Guzzle Version

--- src/Client/CreateClientTrait.php
+++ src/Client/CreateClientTrait.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace Vin\ShopwareSdk\Client;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+
 trait CreateClientTrait
 {
     protected ClientInterface $httpClient;
 
     public function createHttpClient(array $config = []): ClientInterface
     {
-        return $this->httpClient = Client::create($config);
+        return $this->httpClient = new Client($config);
     }
 
     public function getHttpClient(): ClientInterface
--- src/Data/Response/SdkResponse.php
+++ src/Data/Response/SdkResponse.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 namespace Vin\ShopwareSdk\Data\Response;
 
 use GuzzleHttp\Psr7\Response;
-use Psr\Http\Message\ResponseInterface;
 
-class SdkResponse extends Response implements ResponseInterface
+class SdkResponse extends Response
 {
 }
